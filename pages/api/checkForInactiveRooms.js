ff95128ee86b6007288a32f3789adc0521a057d0
kaan <mbalkan@wgu.edu>
2024-02-24 23:10:04 -0800

    minor fixes#2

--- pages/api/checkForInactiveRooms.js
+++ pages/api/checkForInactiveRooms.js
@@ -6,13 +6,13 @@ import Room from "../models/Room";
 export default async function checkForInactiveRooms(req, res) {
   await dbConnect();
 
-  const TIMEOUT = 11 * 1000; // 11 seconds
+  const TIMEOUT = 100 * 1000; // 11 seconds
   const now = new Date();
   const rooms = await Room.find({
     lastActive: { $lt: new Date(now.getTime() - TIMEOUT) },
   });
   for (const room of rooms) {
-    await Room.findByIdAndUpdate(Room._id, {
+    await Room.findByIdAndUpdate(room._id, {
       status: "timed out",
     });
   }
